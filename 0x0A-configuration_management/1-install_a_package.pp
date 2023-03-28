89261120258ec4e4e6ec6ade654c3b632b302888
kinuj980 <kinuthiajohn980@gmail.com>
2023-03-28 18:31:20 +0300

    app

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,6 @@
-# Instalar un paquete
-
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  name     => 'puppet-lint',
-  provider => 'gem',
-  source   => 'http://rubygems.org',
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
